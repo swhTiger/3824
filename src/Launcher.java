30d11d89ca62ec66fa52bdcf808c94a9ad2bcfc5
swhTiger <2551098527@qq.com>
2019-11-01 23:49:48 +0800

    java homework

--- src/Launcher.java
+++ src/Launcher.java
@@ -150,6 +150,6 @@ public class Launcher {
         UIManager.put("OptionPane.buttonFont", new FontUIResource(new Font("", Font.PLAIN, 14)));
         UIManager.put("OptionPane.messageFont", new FontUIResource(new Font("", Font.ITALIC, 14)));
         new Launcher();
-        new Launcher();   //如果不方便开两台电脑，就可以开两个客户端进行测试
+        //new Launcher();   //如果不方便开两台电脑，就可以开两个客户端进行测试
     }
 }
